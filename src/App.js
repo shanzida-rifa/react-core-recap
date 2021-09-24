bf50badc7f4e4485733a7ee98fb33e59cd97c4fb
Shanzida-Rifa <rifashanzida@gmail.com>
2021-09-24 18:37:48 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoadUsers></LoadUsers>
+      <MyComponent brand='Apple' price='5000'></MyComponent>
+      <MyComponent brand='microsoft' price='10000'></MyComponent>
+      <MyComponent brand='Google' price='50'></MyComponent>
+      <MyComponent></MyComponent>
     </div>
   );
 }
 
+function LoadUsers() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+  return (
+    <div>
+      <h1>Users Loaded:{users.length}</h1>
+      {
+        users.map(user => <User name={user.name} phone={user.phone}></User>)
+      }
+    </div>
+  )
+}
+function User(props) {
+  return (
+    <div className='user'>
+      <h2>Name: {props.name}</h2>
+      <p>call me {props.phone}</p>
+    </div>
+  )
+}
+
+function MyComponent(props) {
+  const [points, setPoints] = useState(1);
+  const myStyle = {
+    backgroundColor: 'lightblue',
+    border: '3px solid blue',
+    margin: '10px',
+    padding: '5px',
+    borderRadius: '10px'
+  }
+  const handleAddCart = () => {
+    const newpoints = points * 2;
+    setPoints(newpoints);
+  }
+  return (
+    <div style={myStyle}>
+      <h1>Yo Yo Mama! {props.brand}</h1>
+      <p>asking money price:{props.price}    I have points: {points} </p>
+
+      <button onClick={handleAddCart}>Add to cart</button>
+      <p>I can write my own component</p>
+    </div>
+  )
+}
 export default App;
